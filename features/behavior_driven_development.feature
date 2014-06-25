38513a8352582cddeaa2f18526cb7060464d2d0f
Joshua Kolden <joshua@crackcreative.com>
2014-06-25 09:58:42 -0700

    behave tests, and identity / resource example.

--- features/behavior_driven_development.feature
+++ features/behavior_driven_development.feature
@@ -2,5 +2,5 @@ Feature: Behavior driven development
 
   Scenario: run a simple test
      Given we have a BDD tool installed
-      When we implement a test
+      When we implement 5 tests
       Then a BDD tool will test it for us!
--- features/steps/behavior_driven_development_steps.py
+++ features/steps/behavior_driven_development_steps.py
@@ -1,6 +1,6 @@
 from behave import given, when, then, step
 
-@given('we have behave installed')
+@given('we have a BDD tool installed')
 def step_impl(context):
     pass
 
@@ -9,7 +9,7 @@ def step_impl(context, number):  # -- NOTE: number is converted into integer
     assert number > 1 or number == 0
     context.tests_count = number
 
-@then('behave will test them for us!')
+@then('a BDD tool will test it for us!')
 def step_impl(context):
     assert context.failed is False
     assert context.tests_count >= 0
